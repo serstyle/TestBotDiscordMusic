db2133c51e47de1fb91ef1e257b6aedee7a517ab
Francois Desobry <desobry.f@gmail.com>
2020-06-08 09:36:17 +0200

    playMusic is now a method of Server add getter for Queue

--- helpers/units.ts
+++ helpers/units.ts
@@ -1,9 +1,6 @@
-import { VoiceChannel, Message } from "discord.js";
-import * as ytdl from "ytdl-core";
-import { isQueueEmpty } from "./conditions";
+import { Message } from "discord.js";
 import fetch from "node-fetch";
 import * as config from "../config.json";
-import { ServerDiscord } from "../Models/Server";
 
 export enum MessageCommand {
   PLAY = "!play",
@@ -27,36 +24,6 @@ export const deleteMessage = (message: Message) => {
   }
 };
 
-export const playMusic = (
-  vc: VoiceChannel,
-  message: Message,
-  server: ServerDiscord
-) => {
-  vc.join().then((connection) => {
-    const serverQueue = server.getQueue();
-
-    message.channel.send(
-      `C cette music qui se joue mtn lol: "${serverQueue[0].title}" et le lien c est ${serverQueue[0].url}`
-    );
-
-    // create a stream from the url with YTDL.
-    // uses by the connection.play method.
-    const stream = ytdl(serverQueue[0].url, {
-      filter: "audioonly",
-      highWaterMark: 1 << 25,
-      quality: "highestaudio",
-    });
-
-    server.createDispatcherToConnectPlay(connection, stream);
-
-    server.dispatcherAction("finish", () => {
-      server.removeFromQueue();
-      if (isQueueEmpty(serverQueue)) return;
-      playMusic(vc, message, server);
-    });
-  });
-};
-
 export const getYoutubeUrl = async (keywords: string) => {
   const response = await fetch(
     `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${keywords}&type=video&key=${config.API_KEY_YOUTUBE}`
@@ -65,7 +32,7 @@ export const getYoutubeUrl = async (keywords: string) => {
 
   console.log(keywords);
   console.log("json parse: ".toUpperCase(), youtubeLinks);
-  
+
   const firstYoutubeLink =
     youtubeLinks && youtubeLinks.items.length > 0
       ? `https://www.youtube.com/watch?v=${youtubeLinks.items[0].id.videoId}`
